41af5c84cb0560c043aa8322b25a1f7e14589afd
Pasheza <pashezik@gmail.com>
2016-12-12 13:37:36 +0300

    Admin is added to api permissions

--- api/permissions.py
+++ api/permissions.py
@@ -4,12 +4,14 @@ from finance.models import Account
 
 class IsAccountOwner(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
+        if request.user.is_staff:
+            return True
         return request.user.id == obj.user_id
 
 
 class IsChargeOwner(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
+        if request.user.is_staff:
+            return True
         acc = Account.objects.get(pk=obj.account_id)
         return request.user.id == acc.user_id
-
-
