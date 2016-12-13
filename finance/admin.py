39c362b52c707b91d24447239451f37171780110
Pasheza <pashezik@gmail.com>
2016-12-13 17:58:25 +0300

    Models added to admin page

--- finance/admin.py
+++ finance/admin.py
@@ -1,3 +1,10 @@
-from django.contrib import admin
-
 # Register your models here.
+from django.contrib import admin
+from finance.models import User, Account, Charge, Goal
+
+
+admin.site.register(User)
+admin.site.register(Account)
+admin.site.register(Charge)
+admin.site.register(Goal)
+
