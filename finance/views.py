f6b163e3b1c40c74f1739cebd6855b464641f3b7
Brosha <smileanton@yandex.ru>
2016-12-14 03:08:28 +0300

    wtf

--- finance/views.py
+++ finance/views.py
@@ -86,6 +86,7 @@ def account_status(request, account_id=0):
 @user_check
 @login_required(login_url='login')
 def add_charge(request, account_id=0):
+    request.session['flag'] = False
     if request.method == 'POST':
         form = ChargeForm(request.POST)
         info = 'Form is filled, but not correct'
@@ -279,6 +280,7 @@ def login_view(request):
 
 @login_required(login_url='login')
 def profile(request):
+
     #user_id = request.session['user_id']
     user_id = request.user.id
     print(user_id)
